60046e0bbd2916c64a7abaefdb355ed344897c07
Vien Vu <lamvienbk@gmail.com>
2022-12-03 12:01:39 +0700

    day 1 part 2

--- 2022/AOC2022.playground/Pages/Day1.xcplaygroundpage/Contents.swift
+++ 2022/AOC2022.playground/Pages/Day1.xcplaygroundpage/Contents.swift
@@ -2,12 +2,19 @@
 
 import Foundation
 
+
+//
 if let a = readfile() {
     let elves = a.split(separator: "\n\n")
+    //Part 1
     var sum = 0
     for elf in elves {
         var total = elf.split(separator: "\n").map{ Int($0)! }.reduce(0, { $0 + $1 })
         if total > sum { sum = total }
     }
     print(sum)
+    
+    //Part 2
+    let a = elves.map { $0.split(separator: "\n").map{ Int($0)! }.reduce(0, { $0 + $1 })}.sorted { $0 > $1 }
+    print(a[0] + a[1] + a[2])
 }
